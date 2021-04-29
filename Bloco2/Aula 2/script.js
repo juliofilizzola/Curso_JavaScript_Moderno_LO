8004b21302b01cfeff7f3af5c0922a789e9af352
Julio Filizzola <juliofilizzola@hotmail.com>
2021-04-28 23:21:04 -0300

    first Commit

---
+++ Bloco2/Aula 2/script.js
@@ -0,0 +1,21 @@
+// atividade de fixação do Console.log
+
+// primeiro jeito 
+
+console.log('Meu nome é "Julio Filizzola", Estou estudando às', 23, 'horas');
+
+// segundo jeito
+
+console.log("Meu nome é 'Julio Filizzola', Estou estudando às", 23, "horas");
+
+// terceiro jeito 
+
+console.log(`Meu nome é "Julio Filizzola", Estou estudando às`, 23, `horas`);
+
+// quarto jeito
+
+const name = 'Julio Filizzola';
+const number = 23;
+
+console.log(`Meu nome é '${name}', Estou estudando às ${number} horas`);
+
