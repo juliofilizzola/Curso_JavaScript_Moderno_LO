3263c8514c05e15c4beaf1dca2dc915b8439a5d7
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-03 21:45:03 -0300

    Secao2 aula 10

---
+++ Bloco2/Aula 10/index.js
@@ -0,0 +1,14 @@
+// tipos primitivos 
+
+const nome = 'Julio'; // string
+const anos = 1996; // number
+const fuma = false; // boolean
+let dirigi; // undefined -> não aponta para nenhum local na memoria.
+let bebe = null; // nulo -> também não ponta para nenhum local na memoria.
+
+console.log(`1 ${typeof nome}`);
+console.log(`2 ${typeof anos}`);
+console.log(`3 ${typeof fuma}`);
+console.log(`4 ${typeof dirigi}`);
+console.log(`5 ${typeof bebe}`);
+
