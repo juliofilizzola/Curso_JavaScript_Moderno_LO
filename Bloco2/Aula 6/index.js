03ba1e6721c4612b05395f8bc92e9433d4d69f56
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-03 21:01:12 -0300

    Secao2 aula 6

---
+++ Bloco2/Aula 6/index.js
@@ -0,0 +1,12 @@
+// variaveis 
+let nome = 'João';
+
+console.log(`o ${nome} está doente.`);
+console.log(`o ${nome} está doente, ou não?`);
+console.log(`Xablau, ${nome}!`);
+
+// Não podemos atribuir o mesmo nome ou um nome com palavra resvada para essa variavel 
+// não pode começar com numero. 
+// Sem espaços no nome.
+// uma anotação camleCase;
+// Case-sensitive;
