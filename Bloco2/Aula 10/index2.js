4ee033ac4448facab9f7deca7dd0394cd98e4d32
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-03 21:59:46 -0300

    Secao2 aula 10 part 1

---
+++ Bloco2/Aula 10/index2.js
@@ -0,0 +1,11 @@
+// operadores aritimeticos.
+
+const adicao = 2 + 2;
+const divisao = 2 / 2;
+const subtracao = 2 - 1;
+const multiplicacao = 2 * 2;
+
+console.log(adicao, divisao, subtracao, multiplicacao);
+
+
+
