7250c0e7da45a5ede76d870c77994d4fb64e6953
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-03 21:16:13 -0300

    Secao2 aula 7 details

--- Bloco2/Aula 7/script.js
+++ Bloco2/Aula 7/script.js
@@ -2,3 +2,7 @@
 // const não pode ser rescrita
 // não pode repetir o nome e nem colocar nome reservado. 
 // assim como vimos com let.
+const nomeJ = 'Julio';
+nomeJ = 5/5;// vai dar erro
+
+console.log(nomeJ);// Assignment to constant variable.
