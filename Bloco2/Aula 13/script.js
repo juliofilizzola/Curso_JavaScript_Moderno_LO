abad4e5d9deb87618ecae6240839239635729909
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-06 18:35:22 -0300

    Secao2 String

--- Bloco2/Aula 13/script.js
+++ Bloco2/Aula 13/script.js
@@ -4,3 +4,12 @@ let oneString = 'um texto';
 
 console.log(oneString[1]);
 console.log(oneString.charAt(6));
+// outros metodos 
+console.log(oneString.match(/[a-z]/g));
+console.log(oneString.search(/[a-z]/));
+
+console.log(oneString.replace(/t/g, '#')); // aqui substituimos. 
+
+console.log(oneString.length);// tamanho da string.
+
+
