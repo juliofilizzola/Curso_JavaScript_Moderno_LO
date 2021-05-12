56494a3003d709fe6515bb2534dba380ee2fc7bd
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-11 22:02:39 -0300

    Secao2 atividade Aula 14

---
+++ Bloco2/Aula 14/script.js
@@ -0,0 +1,6 @@
+let numb = 1000;// numero interio
+let numbe = 2.1 //numero de ponto flut
+
+// console.log(numb.toString() + numbe );
+console.log(numb.toString(2));
+console.log(numb.toString(16))
