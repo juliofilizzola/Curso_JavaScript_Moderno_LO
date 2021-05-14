11294a2a7999890a149212b14121df34473f2b63
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-14 14:16:33 -0300

    [Secao/2] Aula 14

--- Bloco2/Aula 14/script.js
+++ Bloco2/Aula 14/script.js
@@ -2,5 +2,20 @@ let numb = 1000;// numero interio
 let numbe = 2.1 //numero de ponto flut
 
 // console.log(numb.toString() + numbe );
-console.log(numb.toString(2));
-console.log(numb.toString(16))
+// console.log(numb.toString(2));
+// console.log(numb.toString(16))
+
+// lidando com numeros com pontos flutuantes. 
+
+// lidar com numero com ponto flutuante,
+
+let num1 = 0.7;
+let num2 = 0.2;
+
+// num1 += num2;
+
+// Multiplicando o numero e dividinto o resultado por 100//
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+
+console.log(num1);
