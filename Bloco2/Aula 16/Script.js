7dd9a2aa74f2e59dde4a32858dcf9d89a93aca86
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-19 21:14:46 -0300

    [Secao/2] atividade Aula 16 begin

---
+++ Bloco2/Aula 16/Script.js
@@ -0,0 +1,17 @@
+// Arrays
+
+const alunos = [
+   {
+    Name: 'Luiz',
+    Age: 19,
+    Repetiu: true,
+  },
+   {
+    Name: 'Renato',
+    Age: 17,
+    Repetiu: false,
+  }
+]; // array de objestos.
+
+
+console.log(alunos[0].Name); // log pegando só um elemento.
