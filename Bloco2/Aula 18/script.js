495cf88a6253e22168dd7a74a3129bbe0a9aeab2
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-26 21:59:14 -0300

    [secao/2] objetos exemplos

---
+++ Bloco2/Aula 18/script.js
@@ -0,0 +1,20 @@
+// exemplos de objetos
+
+const array = {
+  xablau: "Fala",
+  enshinua: 'tribo,',
+  map: 'Beleza?'
+}
+
+const concat = ({xablau, enshinua, map}) => `${xablau} ${enshinua} ${map}`;
+
+console.log(concat(array));
+
+const people = (fistName, lastName, age) => ({fistName, lastName, age});// criando um objeto
+
+// quando deixamos assim a chave com o mesmo nome do que o paramentro que vai ser valor o javascript já entende que é chave e valor.
+
+
+const people1 = people('Luiz', 'Fernando', 23);
+
+console.log(people1);
