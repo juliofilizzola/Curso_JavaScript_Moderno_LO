e3c463c777d064e279fffa0992ec6c2b735679a2
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-05 21:39:47 -0300

    [secao/2] finalizando atividade

--- Bloco2/Aula 20/src/script.js
+++ Bloco2/Aula 20/src/script.js
@@ -1,15 +1,29 @@
 const myScope = () => {
   const forms = document.querySelector('.form');
-  const div = document.querySelector('render')
+  const div = document.querySelector('.render');
+  const people = [];
+
   const submitForm = event => {
     event.preventDefault();
     const name = forms.querySelector('.name');
     const lastname = forms.querySelector('.lastname');
-    const peso = forms.querySelector('.peso');
+    const peso = forms.querySelector('.Peso');
     const altura = forms.querySelector('.altura');
-    
+    people.push({
+      name: name.value,
+      sobrenome: lastname.value,
+      peso:  peso.value,
+      altura: altura.value
+    });
+   div.innerHTML += `<p> ${name.value} </p> 
+   <p> ${lastname.value}</p>
+   <p> ${peso.value}</p>
+   <p> ${altura.value} </p>
+   `
   }
-  forms.addEventListener('submit', submitForm)
+
+  forms.addEventListener('submit', submitForm);
+  
 }
 
 myScope();
