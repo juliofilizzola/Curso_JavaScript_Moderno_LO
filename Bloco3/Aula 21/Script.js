8b8b1af916196b4ac05659dc1355ee949379ca09
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-09 12:31:07 -0300

    [secao/3] - exemplos de desigualdade

--- Bloco3/Aula 21/Script.js
+++ Bloco3/Aula 21/Script.js
@@ -6,6 +6,10 @@ Alguns conparadores
 < menor do que 
 == igual 
 === estritamente igual (essque normalmente usamos);
+>= Maior ou igual 
+<= menor ou igual 
+!== estritamente diferente
+!= diferente
 */
 
 // exemplos 
@@ -17,8 +21,16 @@ console.log(menor); // retorna true;
 
 console.log("------>");
 
-const igual = '1' == 1;
+const igual = '1' == 1;// não utilizamos mais.
 console.log(igual);// retorna true;
 
 const estritamente = '1' === 1;
 console.log(estritamente); // retorna false;
+
+console.log('--->');
+
+const diferente = 23 != "23.0";// a gente não utiliza mais.
+console.log(diferente);// Vai retornar false;
+
+const estrid = 23 !== '23.0';
+console.log(estrid);// vai retornar true;
