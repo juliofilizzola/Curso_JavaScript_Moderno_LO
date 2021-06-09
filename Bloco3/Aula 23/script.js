caf19fa9c4b639a57954d3b513b533211c5c67fc
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-09 19:18:35 -0300

    [secao/3] - comentario de exemplo

--- Bloco3/Aula 23/script.js
+++ Bloco3/Aula 23/script.js
@@ -6,3 +6,9 @@
 // exemplo 
 
 console.log( 'Jõa' && true && 'Maria');
+
+// podemos comparar qualquer tipo de dado;
+
+/** O que aconteceu nesse exemplo foi:
+ * verificou se 'Jõa' é true e depois verificou se true é 'Maria' e retornou o ultimo;
+ */
