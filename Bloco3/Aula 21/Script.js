e3ac8d01539c09fe52d9b79966ec1237b0c19e77
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-09 11:03:24 -0300

    [secao/3] - exemplos de comparadores

--- Bloco3/Aula 21/Script.js
+++ Bloco3/Aula 21/Script.js
@@ -1,2 +1,18 @@
 /*  Intro logica! */
 /* Operadores de comparadores */
+/*
+Alguns conparadores
+> maior que 
+< menor do que 
+== igual 
+=== estritamente igual (essque normalmente usamos);
+*/
+
+// exemplos 
+const maior = 2>1;
+console.log(maior); // retorna true;
+
+const menor = 1<2;
+console.log(menor); // retorna true;
+
+console.log("------>");
