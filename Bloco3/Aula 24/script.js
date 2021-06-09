759c70fcbcb5aefd18272766c85e6b469bc3c611
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-09 20:07:06 -0300

    [secao/3] - comentarios do exemplo

--- Bloco3/Aula 24/script.js
+++ Bloco3/Aula 24/script.js
@@ -7,3 +7,10 @@ if (horas < 12) {
 } else {
   console.log('Boa tarde, já almoçou?');
 }
+
+
+/** 
+// AQUI FOI FEITA UMA COMPORAÇÃO SE A CONST HORAS TEM O VALOR MENOR DO QUE 12
+ se for, ele vai executar o log de bom dia
+ se não ele vai para o log de boa tarde! 
+*/
