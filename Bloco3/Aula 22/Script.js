88437d482d8ef4f0442ec5fbf8116e170b91fa2e
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-09 17:00:35 -0300

    [secao/3] - creat exemplos

--- Bloco3/Aula 22/Script.js
+++ Bloco3/Aula 22/Script.js
@@ -7,3 +7,9 @@
 const numb1 = 1;
 const numb2 = 2;
 
+const tr = numb1 && numb2 === 1;
+const tr2 = numb1 && numb2 === 2;
+
+console.log(tr && tr2);
+console.log(tr || tr2 == true);
+console.log(tr2);
