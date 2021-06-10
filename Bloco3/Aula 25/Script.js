5698a0212f8545b41b7a0c042bb7456cd79f33a0
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-10 11:53:56 -0300

    [secao/3] - exemplo

---
+++ Bloco3/Aula 25/Script.js
@@ -0,0 +1,10 @@
+/** Outro exemplos de If */
+
+const number = 10;
+
+if (number >= 0 && number <= 5 ) {
+  console.log('O número está entre 0 e 5');
+} // não retornou 
+if (number >= 5) {
+  console.log('O numero NÂO está entre 0 e 5');
+}
