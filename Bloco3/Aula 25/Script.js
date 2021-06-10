fd4ae9f85ea8efc0dc837a9ce670a45ec011f1ff
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-10 11:55:38 -0300

    [secao/3] - comentario do exemplo

--- Bloco3/Aula 25/Script.js
+++ Bloco3/Aula 25/Script.js
@@ -7,4 +7,6 @@ if (number >= 0 && number <= 5 ) {
 } // não retornou 
 if (number >= 5) {
   console.log('O numero NÂO está entre 0 e 5');
-}
+}// retornou 
+
+/** Aqui ele executa um bloco se passar na condição, então se desencader um escopo de bloco, automaticamente o proximo não sera executado.  */
