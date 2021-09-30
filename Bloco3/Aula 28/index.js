3398f5db072619ae06c678a12dbbf376e68e568a
filizzola <juliofilizzola@hotmail.com>
2021-09-30 14:01:55 -0300

    exemplo com a data 0

--- Bloco3/Aula 28/index.js
+++ Bloco3/Aula 28/index.js
@@ -6,4 +6,11 @@ console.log(data.toString());
 const TresHoras = 60 * 60 * 3 * 1000;
 const Umdria = 60 * 60 * 24 * 1000;
 
-const data0 = new Data(0);
+const data0 = new Date(0 + TresHoras);
+const data1 = new Date(0);
+
+// hora zero é 01/01/1970 timestamp unix ou época do unix
+
+console.log(data0.toString());
+
+console.log(data1);
