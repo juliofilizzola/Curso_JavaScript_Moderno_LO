4e3ae30b82d7606011a2962c34b68d1cb074bf55
filizzola <juliofilizzola@hotmail.com>
2021-09-30 13:52:21 -0300

    criando exemplos de Data

--- Bloco3/Aula 28/index.js
+++ Bloco3/Aula 28/index.js
@@ -2,3 +2,8 @@ const data = new Date();
 
 console.log(data);
 console.log(data.toString());
+
+const TresHoras = 60 * 60 * 3 * 1000;
+const Umdria = 60 * 60 * 24 * 1000;
+
+const data0 = new Data(0);
